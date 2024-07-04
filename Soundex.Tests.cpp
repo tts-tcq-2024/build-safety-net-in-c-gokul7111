da79f4bd0b59c08e33569744521b3f1cd89e44b9
gokul7111 <gokulvadivel711@gmail.com>
2024-07-04 10:31:31 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,12 +5,12 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_01) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
-  ASSERT_EQ(soundex,"A200");
+  ASSERT_EQ(strcmp(soundex,"A200"), 0);
 }
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_02) {
  //GOKUL
   char soundex[5];
   generateSoundex("gokul", soundex);
-  ASSERT_EQ(soundex,"G240");
+  ASSERT_EQ(strcmp(soundex,"G240"), 0);
 }
