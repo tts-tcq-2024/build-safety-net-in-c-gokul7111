0ab901c487a060aa02217d1eb5b0d9b2156f0f2e
gokul7111 <gokulvadivel711@gmail.com>
2024-07-04 10:21:34 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -8,7 +8,7 @@ static char charList[26] = {'0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2
 
 bool IsCodeValid(char currChar, char prevCode)
 {
-    return (c != '0' && c != prevCode);
+    return (currChar != '0' && currChar != prevCode);
 }
 
 bool IsAlphabet(char c)
