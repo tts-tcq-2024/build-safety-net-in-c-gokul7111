555d9d67b229026ce2dd919a060f5de40a62f407
gokul7111 <gokulvadivel711@gmail.com>
2024-07-04 10:00:49 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,16 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_01) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+  ASSERT_EQ(soundex,"A200");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_02) {
+ //GOKUL
+  char soundex[5];
+  generateSoundex("gokul", soundex);
+  ASSERT_EQ(soundex,"G240");
 }
